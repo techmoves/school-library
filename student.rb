c68349d10c519721b68fbd47952823140956bf2d
techmoves <muhndlovu28@gmail.com>
2023-07-31 19:32:58 +0200

    Removed the id in the super constructer

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 # student.rb
 class Student < Person
   def initialize(id, age, classroom, parent_permission: true, name: 'Unknown')
-    super(id, age, parent_permission: parent_permission, name: name)
+    super( age, parent_permission: parent_permission, name: name)
     @classroom = classroom
   end
 
