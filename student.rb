d05cf9a07b6c190bc0f37aab84b282c0eec67801
techmoves <muhndlovu28@gmail.com>
2023-07-31 19:35:02 +0200

    Fix linters error

--- student.rb
+++ student.rb
@@ -2,8 +2,8 @@ require_relative 'person'
 
 # student.rb
 class Student < Person
-  def initialize(id, age, classroom, parent_permission: true, name: 'Unknown')
-    super( age, parent_permission: parent_permission, name: name)
+  def initialize(_id, age, classroom, parent_permission: true, name: 'Unknown')
+    super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 # teacher.rb
 class Teacher < Person
-  def initialize(id, age, specialization, parent_permission: true, name: 'Unknown')
+  def initialize(_id, age, specialization, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
     @specialization = specialization
   end
