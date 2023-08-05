93a30668b5965b16831f04f94a67e4a303ab9c47
techmoves <muhndlovu28@gmail.com>
2023-08-05 02:33:28 +0200

    Fix the bug

--- person.rb
+++ person.rb
@@ -5,16 +5,14 @@ class Person < Nameable
   attr_accessor :name, :age, :rental
   attr_reader :id
 
-  # rubocop:disable Lint/MissingSuper
   def initialize(name, age)
-    # super
+    super
     @id = Random.rand(1..200)
     @name = name
     @age = age
     @rentals = []
   end
 
-  # rubocop:enable Lint/MissingSuper
   def add_rental(rental)
     @rental.push(rental)
     rental.person = self
